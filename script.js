3340bbe9a87fc262b2eee182960d7ce986abe0be
Masana Chauke <masanaapril@gmail.com>
2023-04-12 08:44:22 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function getFormValues(){
+    let name = document.getElementById("fname").value;
+    let surname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let occupation = document.getElementById("occupation").value;
+
+    console.log(name);
+    console.log(surname);
+    console.log(email);
+    console.log(occupation);
+}
+   
